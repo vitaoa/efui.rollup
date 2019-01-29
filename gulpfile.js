4a14d93044bffb36cf952c25cce78f180a186f05
vitaoa <jupiterzw@sina.com>
2019-01-29 17:27:50 +0800

    master:dev # Tag: #    1.开发环境：图片优化处理； #    2.JS：新增事件绑定，索引； #    3.插件：新增轮播图
    # Conflicts:
    #	app/views/plugin.html

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const sass = require('gulp-sass');
 const rollup = require('gulp-rollup');
 const typescript = require('rollup-plugin-typescript');
 const base64 = require('gulp-base64');
+const tinypng_nokey = require('gulp-tinypng-nokey');   //压缩图片 免费 不限制压缩次数，模拟用户上传和下载的行为
+const glob = require("glob");
+const path = require("path");
+const spritesmith = require('gulp.spritesmith');
 
 //---------------------------------------参数声明----------------------------//
 const fileinclude_DIR = './app/';   // 源文件目录
@@ -53,6 +57,85 @@ gulp.task('copy:datas',function () {
 gulp.task('copy:js',function () {
     return gulp.src(fileinclude_DIR + 'scripts/lib/**/*.js')
         .pipe(gulp.dest(DIST_DIR + 'js/lib/'));
+});
+gulp.task('copy:images',function () {
+    return gulp.src([fileinclude_DIR + 'images/**/*.{png,jpg,gif,jpeg}','!' + fileinclude_DIR + 'images/base64/**/*.*','!' + fileinclude_DIR + 'images/slice/**/*.*'])
+        .pipe(gulp.dest(DIST_DIR + 'images/'));
+});
+gulp.task('images:tinypng', gulp.series('copy:images',function() {
+    return gulp.src(DIST_DIR + 'images/*.{png,jpg,gif,jpeg}')
+        .pipe(tinypng_nokey()).on('error', function(err) {
+            console.error(err.message);
+        })
+        .pipe(gulp.dest(DIST_DIR + "images/"));
+}));
+
+// sprites
+let spiresTime = (Math.random()*1000).toFixed(0);
+const REMwidth = 750; //移动端设计稿宽
+gulp.task('sprites:more',function(done){
+    let dirs = glob.sync(fileinclude_DIR + "images/slice/*/*/");
+    for(let i = 0;i<dirs.length;i++){
+        let baseDir = dirs[i].split("/")[dirs[i].split("/").length-3];
+        let basename = path.basename(dirs[i]);
+        console.log(baseDir);
+        console.log(basename);
+        let spriteData = gulp.src(fileinclude_DIR + `images/slice/${baseDir}/${basename}/*.{png,jpg,gif,jpeg}`)//需要合并的图片地址
+            .pipe(spritesmith({
+                imgName: `sp_${baseDir}_${basename}.png`,//保存合并后图片的地址
+                cssName: `sp-${baseDir}-${basename}.scss`,//保存合并后对于css样式的地址
+                padding:10,//合并时两个图片的间距
+                algorithm: 'top-down',
+                cssTemplate: function(data){
+                    let arr1,arr2,width1,width2,height1,height2;
+                    let arr = ["/* ============ sprites ============ */","\n"],
+                        width = data.spritesheet.px.width,
+                        height = data.spritesheet.px.height,
+                        url =  data.spritesheet.image;
+
+                    let mVar = data.spritesheet.image.split("/")[data.spritesheet.image.split("/").length-1].replace(/-m_/,'_m_').indexOf('_m');
+                    data.sprites.forEach(function(sprite) {
+                        arr.push(
+                            "."+sprite.name+
+                            "{ "+
+                            "width: "+sprite.px.width+";"+
+                            "height: "+sprite.px.height+";"+
+                            "background-image: url('"+url+"?="+spiresTime+"');"+
+                            "background-position:"+sprite.px.offset_x+" "+sprite.px.offset_y+";"+
+                            "background-size: "+ width+" "+height+";"+
+                            "background-repeat:no-repeat;"+
+                            " }\n"
+                        );
+                    });
+                    if(mVar!==-1){
+                        arr1 = ["\n","/* ============ sprites rem ============ */","\n"];
+                        width1 = data.spritesheet.rem.width;
+                        height1 = data.spritesheet.rem.height;
+
+                        arr1.push('@media(max-width:'+REMwidth+'px){\n');
+                        data.sprites.forEach(function(sprite) {
+                            arr1.push(
+                                "."+sprite.name+
+                                "{ "+
+                                "background-position:"+sprite.rem.offset_x+" "+sprite.rem.offset_y+";"+
+                                "background-size: "+ width1+" "+height1+";"+
+                                "width: "+sprite.rem.width+";"+
+                                "height: "+sprite.rem.height+";"+
+                                " }\n"
+                            );
+                        });
+                        arr1.push('}');
+                        return arr.join("")+(arr1.join(""));
+                    }
+                    return arr.join("");
+                },
+                imgPath: `../images/sp_${baseDir}_${basename}.png`,
+                spritestamp: true
+            }));
+        spriteData.img.pipe(gulp.dest(DIST_DIR + "images"));
+        spriteData.css.pipe(gulp.dest(fileinclude_DIR + 'styles/sprites/'));
+    }
+    done();
 });
 //---------------------------------------file-include----------------------------//
 
@@ -121,4 +204,5 @@ gulp.task('server',gulp.series('fileInclude', 'fileIncludeIndex','copy:datas','c
     gulp.watch(fileinclude_DIR + 'views/**/*.*',gulp.parallel('fileInclude', 'fileIncludeIndex'));
     gulp.watch(fileinclude_DIR + 'scripts/**/*.*',gulp.parallel('copy:datas','copy:js','rollup:js'));
     gulp.watch(fileinclude_DIR + 'styles/**/*.*',gulp.series('sass'));
+    gulp.watch(fileinclude_DIR + 'images/**/*.*',gulp.series('images:tinypng'));
 }));
